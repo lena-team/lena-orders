8791046bd477d343a1a922bdc677a316ea3e641e
Austin Sloane <sloane.ats@gmail.com>
2017-10-24 12:28:39 -0700

    added basic server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
+// constants
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Start the server
+app.listen((port, () => {
+  console.log(`Listening on port: ${port}!`);
+}));
