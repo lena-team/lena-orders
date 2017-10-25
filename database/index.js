dbb458e891b019969a4fe83aaf44f30439b732c0
Austin Sloane <sloane.ats@gmail.com>
2017-10-25 11:45:22 -0700

    add basic queries to be used in routes

--- database/index.js
+++ database/index.js
@@ -1,25 +1,63 @@
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    user: process.env.LENA_ORDERS_DB_USER || 'lena',
-    host: process.env.LENA_ORDERS_DB_HOST || 'localhost',
-    password: process.env.LENA_ORDERS_DB_PASS,
-    database: process.env.LENA_ORDERS_DB_data || 'transactions',
-    port: process.env.LENA_ORDERS_DB_PORT || 5432,
-  },
+const { Client } = require('pg');
+const Promise = require('bluebird');
+
+const client = new Client({
+  user: process.env.LENA_ORDERS_DB_USER || 'lena',
+  host: process.env.LENA_ORDERS_DB_HOST || 'localhost',
+  database: process.env.LENA_ORDERS_DB_data || 'transactions',
+  port: process.env.LENA_ORDERS_DB_PORT || 5432,
 });
 
-const bookshelf = require('bookshelf')(knex);
+client.connect();
 
-const Order = bookshelf.Model.extend({
-  tableName: 'orders',
-});
-
-Order.fetchAll()
-  .then((response) => {
-    const { models } = response;
-    models.forEach(model => console.log(model));
-  })
-  .catch((err) => {
-    console.error('ERROR', err);
+const createOrder = (userId, shippingAddress) => {
+  const queryStr = `
+    INSERT INTO orders
+    (user_id, shipping_address, delivery_status, created_at)
+    VALUES
+    ($1, $2, 'created', $3)
+  `;
+  const currDate = new Date().toISOString().slice(0, 10);
+  const queryArgs = [userId, shippingAddress, currDate];
+  return new Promise((resolve, reject) => {
+    client.query(queryStr, queryArgs, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
   });
+};
+
+const updateOrderStatus = (orderId, deliveryStatus, updatedAt) => {
+  const queryStr = `
+    UPDATE orders
+    SET
+    delivery_status = $2, updated_at = $3
+    WHERE order_id = $1
+  `;
+  const queryArgs = [orderId, deliveryStatus, updatedAt];
+  return new Promise((resolve, reject) => {
+    client.query(queryStr, queryArgs, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+};
+
+// createOrder(2, 'work')
+//   .then(result => console.log(result))
+//   .catch(err => console.error(err));
+//
+// updateOrderStatus(3, 'completed', new Date().toISOString().slice(0, 10))
+//   .then(result => console.log(result))
+//   .catch(err => console.error(err));
+
+module.exports = {
+  createOrder,
+  updateOrderStatus,
+};
