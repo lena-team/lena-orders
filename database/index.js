6da52d901c3f6a9f1421b35e64d835ec9d4c289f
Austin Sloane <sloane.ats@gmail.com>
2017-11-03 15:15:09 -0700

    added comments describing functions

--- database/index.js
+++ database/index.js
@@ -1,5 +1,4 @@
 const pgp = require('pg-promise');
-const yyyymmdd = require('yyyy-mm-dd');
 
 const connection = {
   host: process.env.LENA_ORDERS_DB_HOST || 'localhost',
@@ -10,6 +9,14 @@ const connection = {
 };
 
 const db = pgp(connection);
+
+/**
+ * Will create an order in the `order` table
+ * @param  {index} userId            an integer representing the idenitfying index of a user
+ * @param  {address} shippingAddress an address that the products are to be shipped to
+ * @param  {date} createdAt          date that the order was created on
+ * @return {Promise}                 a promise hodling the result of the row insertion
+ */
 
 const createOrder = (userId, shippingAddress, createdAt) => {
   const queryStr = `
@@ -22,6 +29,14 @@ const createOrder = (userId, shippingAddress, createdAt) => {
   return db.none(queryStr, queryArgs);
 };
 
+/**
+ * Will update the status of an order with the date, and status ('cancelled' or 'delivered')
+ * @param  {number} orderId        the index of the order in the `orders` table
+ * @param  {string} deliveryStatus string indicating status of order ('cancelled' or 'devlivered')
+ * @param  {date} updatedAt        date of update in YYYY-MM-DD format
+ * @return {Promise}               A promise containing the results of the row update
+ */
+
 const updateOrderStatus = (orderId, deliveryStatus, updatedAt) => {
   const queryStr = `
     UPDATE orders
@@ -31,6 +46,21 @@ const updateOrderStatus = (orderId, deliveryStatus, updatedAt) => {
   `;
   const queryArgs = [orderId, deliveryStatus, updatedAt];
   return db.none(queryStr, queryArgs);
+};
+
+/**
+ * A function that will assign
+ * @param {[type]} orderId    [description]
+ * @param {[type]} productArr [description]
+ */
+
+const addProductsToOrder = (orderId, productArr) => {
+  const queryStr = `
+    INSERT INTO products
+    (order_id, product_id, amount)
+    VALUES
+    ($1, $2)
+  `;
 };
 
 // createOrder(2, 'work')
